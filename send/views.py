237b678db9fd6b3875ad7711cbe5317b34164476
Crazy-C0der <sh2000.cool.j@gmail.com>
2021-02-17 13:05:51 +0530

    modal added

--- send/views.py
+++ send/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Transaction, Users
 
 
 # Create your views here.
 def transfer(request):
-    return render(request, 'userlist.html')
+    t = Users.objects.all()
+    return render(request, 'send/first.html', {'user':t})
 
 
 def view(request):
-    return render(request, 'allrecords.html')   
+    return render(request, 'send/allrecords.html')   
